8b91b07ad00a3e0e1c138275636046274a172ee0
Q <yarik.kabashov@yandex.ru>
2025-12-08 22:15:32 +0300

    Added :hover effect to skills cards, edit padding to blocks, added function to download cv, added experience block

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('burger-btn').addEventListener('click', () => {
 	document.querySelector('.header__burgerMenu').classList.toggle('is-active')
 })
+
+document.getElementById('downloadButton').addEventListener('click', () => {
+	const link = document.createElement('a')
+	link.href = 'assets/cv/kabashov-cv.pdf' // Укажите путь к вашему файлу
+	link.download = 'kabashov-cv.pdf' // Имя, под которым файл будет сохранен
+	document.body.appendChild(link)
+	link.click()
+	document.body.removeChild(link)
+})
