47b571ce50e747d3951d385686582d4773989331
Q <yarik.kabashov@yandex.ru>
2025-12-09 14:42:43 +0300

    Added responsive design to experience block, anchor transition function to burger menu, fixed styles of all blocks

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const navLinks = document.querySelectorAll('.header__navLink')
+const burgerMenu = document.querySelector('.header__burgerMenu')
+
+navLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		// Закрыть бургер-меню
+		burgerMenu.classList.toggle('is-active')
+	})
+})
+
 document.getElementById('burger-btn').addEventListener('click', () => {
-	document.querySelector('.header__burgerMenu').classList.toggle('is-active')
+	burgerMenu.classList.toggle('is-active')
 })
 
 document.getElementById('downloadButton').addEventListener('click', () => {
 	const link = document.createElement('a')
-	link.href = 'assets/cv/kabashov-cv.pdf' // Укажите путь к вашему файлу
-	link.download = 'kabashov-cv.pdf' // Имя, под которым файл будет сохранен
+	link.href = 'assets/cv/kabashov-cv.pdf'
+	link.download = 'kabashov-cv.pdf'
 	document.body.appendChild(link)
 	link.click()
 	document.body.removeChild(link)
